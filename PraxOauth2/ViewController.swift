d202ed32a2c4fb2782d8d76d48cfcfbffa052854
John Elmer <canfield@Stiles.local>
2014-06-16 18:21:36 -0400

    Initial Commit

---
+++ PraxOauth2/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  PraxOauth2
+//
+//  Created by John Elmer on 6/16/14.
+//  Copyright (c) 2014 ElmerCat. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+                                    
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+                                    
+    }
+
+
+}
+
---
+++ PraxOauth2/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  PraxOauth2
+//
+//  Created by John Elmer on 6/16/14.
+//  Copyright (c) 2014 ElmerCat. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
